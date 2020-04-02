34c03eea3c627a00e29842f09bdede3d6fa8c3c0
xinggevip <1511844263@qq.com>
2020-04-02 12:28:49 +0800

    自动填充优化

--- mpdeepstudy/src/main/java/com/mp/compont/MyMeteObjectHandle.java
+++ mpdeepstudy/src/main/java/com/mp/compont/MyMeteObjectHandle.java
@@ -10,18 +10,28 @@ import java.time.LocalDateTime;
 class MyMateObjectHandler implements MetaObjectHandler {
     /**
      * setFieldValByName()既可以在insertFill，也可以在updateFill
+     * 自动填充优化
      * @param metaObject
      */
     @Override
     public void insertFill(MetaObject metaObject) {
-        System.out.println("insertFill=================");
-        setInsertFieldValByName("createTime", LocalDateTime.now(), metaObject);
+        // 判断是否有此字段，有则执行填充，无则不执行
+        boolean hasSetter = metaObject.hasSetter("createTime");
+        if (hasSetter) {
+            System.out.println("insertFill=================");
+            setInsertFieldValByName("createTime", LocalDateTime.now(), metaObject);
+        }
 
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        System.out.println("updateFill=================");
-        setUpdateFieldValByName("updateTime", LocalDateTime.now(), metaObject);
+        // 如果用户没设置值则填充，否则不填充
+        Object val = getFieldValByName("updateTime", metaObject);
+        if (val == null) {
+            System.out.println("updateFill=================");
+            setUpdateFieldValByName("updateTime", LocalDateTime.now(), metaObject);
+        }
+
     }
 }
