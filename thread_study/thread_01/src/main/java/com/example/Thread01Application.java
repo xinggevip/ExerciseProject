9b5f3bc52eeb7a6f04b95db07d892eabd1074bf8
xinggevip <1511844263@qq.com>
2020-04-13 09:09:51 +0800

    Thread学习准备文件

---
+++ thread_study/thread_01/src/main/java/com/example/Thread01Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Thread01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Thread01Application.class, args);
+    }
+
+}
