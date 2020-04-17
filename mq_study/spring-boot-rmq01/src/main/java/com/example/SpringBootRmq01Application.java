f5354aefd009f752a57cd4fa43c98af5c34d3076
xinggevip <1511844263@qq.com>
2020-04-17 20:34:04 +0800

    SpringBoot-高级-RMQ-基本环境搭建

---
+++ mq_study/spring-boot-rmq01/src/main/java/com/example/SpringBootRmq01Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRmq01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRmq01Application.class, args);
+    }
+
+}
