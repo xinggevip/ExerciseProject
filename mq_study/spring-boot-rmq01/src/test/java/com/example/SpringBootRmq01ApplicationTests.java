77a178d6f5a2198168b671e16fc983ab9aaa2ca3
xinggevip <1511844263@qq.com>
2020-04-17 20:59:57 +0800

    SpringBoot-高级-RMQ-发送接收消息&序列化

--- mq_study/spring-boot-rmq01/src/test/java/com/example/SpringBootRmq01ApplicationTests.java
+++ mq_study/spring-boot-rmq01/src/test/java/com/example/SpringBootRmq01ApplicationTests.java
@@ -1,13 +1,51 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest
 class SpringBootRmq01ApplicationTests {
 
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    /**
+     * 点对点
+     */
     @Test
     void contextLoads() {
+        // Message需要自己构建一个；定义消息体内容和消息头
+        // rabbitTemplate.send(exchange, routingKey, message);
+        // Object 默认当成消息体，只需要传入要发送的对象，自动化序列发送给rabbitmq；
+        Map<String,Object> map = new HashMap<>();
+        map.put("msg", "这是第一个信息");
+        map.put("data", Arrays.asList("HelloWorld", 666, true));
+        //对象被默认序列以后发送出去
+        rabbitTemplate.convertAndSend("exchange.direct","atguigu",map);
+    }
+
+    /**
+     * 取指定消息队列消息
+     */
+    @Test
+    public void receiveAndConvert(){
+        Object o = rabbitTemplate.receiveAndConvert("atguigu");
+        System.out.println(o.getClass());
+        System.out.println(o);
+    }
+
+    /**
+     * 广播
+     */
+    @Test
+    void sendMessageToAll() {
+        rabbitTemplate.convertAndSend("exchange.fanout","","广播1");
     }
 
 }
