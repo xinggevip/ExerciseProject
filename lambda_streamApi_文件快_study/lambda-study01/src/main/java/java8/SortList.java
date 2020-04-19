9634ed2e6f4df56153b550d33679e19fd8a0c374
xinggevip <1511844263@qq.com>
2020-04-19 20:14:37 +0800

    自定义Comparator排序

--- lambda_streamApi_文件快_study/lambda-study01/src/main/java/java8/SortList.java
+++ lambda_streamApi_文件快_study/lambda-study01/src/main/java/java8/SortList.java
@@ -59,9 +59,44 @@ public class SortList {
         //先是倒序（加reserved），然后正序
         //先是正序（加reserved），然后倒序（加reserved）
     }
+    // 3.自定义Comparator排序
+    private void sortTest03() {
+        Employee e1 = new Employee(1,23,"M","Rick","Beethovan");
+        Employee e2 = new Employee(2,13,"F","Martina","Hengis");
+        Employee e3 = new Employee(3,43,"M","Ricky","Martin");
+        Employee e4 = new Employee(4,26,"M","Jon","Lowman");
+        Employee e5 = new Employee(5,19,"F","Cristine","Maria");
+        Employee e6 = new Employee(6,15,"M","David","Feezor");
+        Employee e7 = new Employee(7,68,"F","Melissa","Roy");
+        Employee e8 = new Employee(8,79,"M","Alex","Gussin");
+        Employee e9 = new Employee(9,15,"F","Neetu","Singh");
+        Employee e10 = new Employee(10,45,"M","Naveen","Jain");
+
+        List<Employee> employees = Arrays.asList(e1, e2, e3, e4, e5, e6, e7, e8, e9, e10);
+
+//        employees.sort(new Comparator<Employee>() {
+//            @Override
+//            public int compare(Employee em1, Employee em2) {
+//                if(em1.getAge() == em2.getAge()){
+//                    return 0;
+//                }
+//                return em1.getAge() - em2.getAge() > 0 ? -1:1;
+//            }
+//        });
+//        employees.forEach(System.out::println);
+
+        employees.sort((em1,em2) -> {
+            if(em1.getAge() == em2.getAge()){
+                return 0;
+            }
+            return em1.getAge() - em2.getAge() > 0 ? -1:1;
+        });
+        employees.forEach(System.out::println);
+    }
     public static void main(String[] args) {
         SortList s1 = new SortList();
-        s1.sortTest01();
-        s1.sortTest02();
+//        s1.sortTest01();
+//        s1.sortTest02();
+        s1.sortTest03();
     }
 }
