a9bbb7d8f0c833605f92823d238ee736a3ed09e7
xinggevip <1511844263@qq.com>
2020-07-20 18:27:20 +0800

    Rmq updata

--- mq_study/spring-boot-rmq01/src/main/java/com/example/service/BookService.java
+++ mq_study/spring-boot-rmq01/src/main/java/com/example/service/BookService.java
@@ -1,9 +1,13 @@
 package com.example.service;
 
 import com.example.entity.Book;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 
 /**
  * @author xinggevip
@@ -11,6 +15,9 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class BookService {
+
+    private Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
     @RabbitListener(queues = "atguigu.news")
     public void receive(Book book) {
         System.out.println("收到消息" + book);
@@ -18,7 +25,9 @@ public class BookService {
 
     @RabbitListener(queues = "atguigu")
     public void receive02(Message message){
-        System.out.println(message.getBody());
+        byte[] body = message.getBody();
+        HashMap<String,Object> hashMap = gson.fromJson(new String(body), HashMap.class);
+        System.out.println(hashMap);
         System.out.println(message.getMessageProperties());
     }
 }
